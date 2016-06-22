d0170035e0bb31057f525e6a36af1276171f7f36
Nabil Ayouchi <nabil@geniusdp.com>
2016-06-22 14:38:45 +0000

    Unused code

--- webroot/theme/js/builder.js
+++ webroot/theme/js/builder.js
@@ -1,33 +1,4 @@
-/**
- * Created by Nabil on 12/06/15.
- */
-
 $(document).ready(function() {
-
-    $('#email-list li > .star > a').on('click', function() {
-        $(this).toggleClass('starred');
-    });
-
-
-    $(".clickable-row > div:not(.chbox,.star)").click(function(e) {
-        if ((e.target instanceof HTMLAnchorElement) == true) {
-            return;
-        }
-        var href = $(this).parent().data('href');
-        if (href != '' && typeof href != 'undefined') {
-            window.document.location = href;
-        }
-    });
-
-
-    /*
-    *
-    * DRAG SYSTEME GENIUS HOLDING
-    *
-    */
-
-
-
     //Init
     var drag_element_id = '';
 
